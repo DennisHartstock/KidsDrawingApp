90f10bf8ac14a24b2f4972652f8cf327b07326e4
DennsHartstock <dennis.hartstock@web.de>
2023-01-26 10:49:58 +0100

    implement showBrushSizeChooserDialog()

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawingView: DrawingView
+    private lateinit var ibBrush: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
 
+        drawingView.setNewBrushSize(10.toFloat())
+        ibBrush.setOnClickListener { showBrushSizeChooserDialog() }
+    }
+
+    private fun initViews() {
         drawingView = findViewById(R.id.vDrawing)
-        drawingView.setNewBrushSize(20.toFloat())
+        ibBrush = findViewById(R.id.ibBrush)
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Choose brush size")
+
+        val ibSmall = brushDialog.findViewById<ImageButton>(R.id.ibSmall)
+        ibSmall.setOnClickListener {
+            drawingView.setNewBrushSize(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val ibMedium = brushDialog.findViewById<ImageButton>(R.id.ibMedium)
+        ibMedium.setOnClickListener {
+            drawingView.setNewBrushSize(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val ibLarge = brushDialog.findViewById<ImageButton>(R.id.ibLarge)
+        ibLarge.setOnClickListener {
+            drawingView.setNewBrushSize(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
