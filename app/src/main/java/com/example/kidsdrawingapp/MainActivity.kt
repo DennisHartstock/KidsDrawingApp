019d71b0b0e389fda57532f8752fe99a5e38bcc9
DennsHartstock <dennis.hartstock@web.de>
2023-01-27 09:24:46 +0100

    add llColors

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.kidsdrawingapp
 import android.app.Dialog
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawingView: DrawingView
+    private lateinit var llColors: LinearLayout
+    private lateinit var ibCurrentColor: ImageButton
     private lateinit var ibBrush: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
 
+        ibCurrentColor = llColors[0] as ImageButton
+        ibCurrentColor.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.color_selected))
         drawingView.setNewBrushSize(10.toFloat())
         ibBrush.setOnClickListener { showBrushSizeChooserDialog() }
     }
 
     private fun initViews() {
         drawingView = findViewById(R.id.vDrawing)
+        llColors = findViewById(R.id.llColors)
         ibBrush = findViewById(R.id.ibBrush)
     }
 
